306182fdb4fb1f007977fd4319144cbb33ecf8d4
Gigirii <vgv888@rambler.ru>
2021-09-25 22:35:54 +0300

    Initial commit

---
+++ src/main/java/HomeWork1.java
@@ -0,0 +1,48 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+        byte byte1 = 100;
+        System.out.println(byte1);
+        short short1 = 12557;
+        System.out.println(short1);
+        int int1 = 1457814;
+        System.out.println(int1);
+        long long1 = 1455745455;
+        System.out.println(long1);
+        float float1 = 1.2f;
+        System.out.println(float1);
+        double double1 = 2.6;
+        System.out.println(double1);
+        char char1 = 'f';
+        System.out.println(char1);
+        String string1 = "Vasya";
+        System.out.println(string1);
+
+
+        int a = 5;
+        int b = 10;
+        int c = 60;
+        int d = 30;
+
+
+        int f = a * (b + (c / d));
+
+
+        System.out.println(f);
+        int a1 = 10;
+        int b1 = 8;
+        int c1 = a1 + b1;
+        if (c1 < 10 && c1 > 20) {
+            System.out.println("Folse");
+        } else System.out.print("True");
+    }
+}
+
+
+
+
+
+
+
+
+
